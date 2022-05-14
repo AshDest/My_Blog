3a8e4d0e62264a154026c975b1e0fc1e4b7dc334
unknown <ashuzamaheshe4@gmail.com>
2022-05-14 13:32:58 +0200

    other commit

--- app/Http/Livewire/Counter.php
+++ app/Http/Livewire/Counter.php
@@ -6,14 +6,20 @@ use Livewire\Component;
 
 class Counter extends Component
 {
+    public $count = 0;
+
     public function render()
     {
-        return view('livewire.counter');
+        return view('livewire.counter', ['counter' => $this->count]);
     }
 
     public function increment()
     {
-
+        $this->count++;
+    }
+    public function decrement()
+    {
+        $this->count--;
     }
 }
 
--- resources/views/livewire/counter.blade.php
+++ resources/views/livewire/counter.blade.php
@@ -1,4 +1,5 @@
 <div style="text-align: center">
-    <button>+</button>
-    <button>-</button>
+    <button wire:click='increment'>+</button>
+    <h1>{{$count}}</h1>
+    <button wire:click='decrement'>-</button>
 </div>
