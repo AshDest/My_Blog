138ba12b6908135795d8a17016fad88e5830ac74
unknown <ashuzamaheshe4@gmail.com>
2022-05-21 17:15:19 +0200

    other commit

--- resources/views/livewire/counter.blade.php
+++ resources/views/livewire/counter.blade.php
@@ -5,8 +5,14 @@
             <input type="text" class="w-full rounded border shadow p-2 mr-2 my-2" placeholder="what your comments">
             <div class="py-2">
                 <button class="p-2 bg-blue-500 w-20 rounded shadow text-white">Add</button>
-
             </div>
+        </div>
+        <div class="rounded border shadow p-3 my-2">
+            <div class="flex justify-start my-2">
+                <p class="font-bold text-lg">Sarthak</p>
+                <p class="mx-3 py-1 text-gray-500 font-semibold">3 min ago</p>
+            </div>
+            <p class="text-gray-800">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Consectetur obcaecati odio</p>
         </div>
     </div>
 </div>
