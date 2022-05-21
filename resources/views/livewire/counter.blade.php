3c9f7e4e023ef2a4ff804ebec955375881b8f17c
unknown <ashuzamaheshe4@gmail.com>
2022-05-21 17:08:18 +0200

    other commit

--- resources/views/livewire/counter.blade.php
+++ resources/views/livewire/counter.blade.php
@@ -1,5 +1,12 @@
 <div style="text-align: center">
-    <button wire:click='increment'>+</button>
-    <h1>{{$count}}</h1>
-    <button wire:click='decrement'>-</button>
+    <div class="w-6/12">
+        <h1 class="my-10 text-3xl">Comments</h1>
+        <div class="my-4 flex">
+            <input type="text" class="w-full rounded border shadow p-2 mr-2 my-2" placeholder="what your comments">
+            <div class="py-2">
+                <button class="p-2 bg-blue-500 w-20 rounded shadow text-white">Add</button>
+
+            </div>
+        </div>
+    </div>
 </div>
